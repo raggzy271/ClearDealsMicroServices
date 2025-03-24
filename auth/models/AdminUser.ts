751f261504da51496636c6b844bb170d22278c1a
Raghav <70934347+raggzy271@users.noreply.github.com>
2025-03-24 21:17:28 +0530

    Replace User.ts by AdminUser.ts

---
+++ auth/models/AdminUser.ts
@@ -0,0 +1,85 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+import sequelize from '../config/db';
+
+interface AdminUserAttributes {
+  id: number;
+  fullName: string;
+  email: string;
+  username: string;
+  contactNo?: string | null;
+  password: string;
+  city: string;
+  popup?: string | null;
+  privilege?: string | null;
+  status: '0' | '1';
+}
+
+interface AdminUserCreationAttributes extends Optional<AdminUserAttributes, 'id'> {}
+
+class AdminUser extends Model<AdminUserAttributes, AdminUserCreationAttributes> implements AdminUserAttributes {
+  public id!: number;
+  public fullName!: string;
+  public email!: string;
+  public username!: string;
+  public contactNo!: string | null;
+  public password!: string;
+  public city!: string;
+  public popup!: string | null;
+  public privilege!: string | null;
+  public status!: '0' | '1';
+}
+
+AdminUser.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    fullName: {
+      type: DataTypes.STRING(200),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(200),
+      allowNull: false,
+      unique: true,
+    },
+    username: {
+      type: DataTypes.STRING(200),
+      allowNull: false,
+      unique: true,
+    },
+    contactNo: {
+      type: DataTypes.STRING(200),
+      allowNull: true,
+    },
+    password: {
+      type: DataTypes.STRING(200),
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING(200),
+      allowNull: false,
+    },
+    popup: {
+      type: DataTypes.STRING(200),
+      allowNull: true,
+    },
+    privilege: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    status: {
+      type: DataTypes.ENUM('0', '1'),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'clear_adminuser',
+    timestamps: false
+  }
+);
+
+export default AdminUser;
