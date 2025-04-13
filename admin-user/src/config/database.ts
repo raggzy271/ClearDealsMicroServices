33fbd26e5e8f36cbdcbdfbae394f5986dbaf3a79
Raghav <70934347+raggzy271@users.noreply.github.com>
2025-04-13 17:39:55 +0530

    add port number to db

--- admin-user/src/config/database.ts
+++ admin-user/src/config/database.ts
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT as string),
         dialect: 'mysql',
         logging: false
     }
